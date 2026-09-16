fcc9e66cdfc09727d13613383eba7acf154c2ff3
Aniket Kadam <anikadamg@gmail.com>
2026-09-16 14:13:31 +0530

    Added a cell

--- app/src/main/java/com/aniketkadam/tictactoe/MainActivity.kt
+++ app/src/main/java/com/aniketkadam/tictactoe/MainActivity.kt
@@ -1,16 +1,26 @@
 package com.aniketkadam.tictactoe
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.TextAutoSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.aniketkadam.tictactoe.ui.theme.TicTacToeTheme
 
 class MainActivity : ComponentActivity() {
@@ -46,3 +56,38 @@ fun GreetingPreview() {
     }
 }
 
+@Preview(showBackground = true)
+@Composable
+fun CellPreview() {
+    TicTacToeCell(CellValue.O)
+}
+
+enum class CellValue {
+    X,
+    O,
+    Empty
+}
+
+@Composable
+fun TicTacToeCell(cellValue: CellValue) {
+    fun getTextValueForCell(cellValue: CellValue): String = when (cellValue) {
+        CellValue.X -> "X"
+        CellValue.O -> "O"
+        CellValue.Empty -> ""
+    }
+
+    Box(
+        modifier = Modifier
+            .size(100.dp)
+            .border(width = 2.dp, color = androidx.compose.ui.graphics.Color(Color.BLUE)),
+        Alignment.Center
+    ) {
+        // Your content here
+        val cellTextValue by remember(cellValue) {
+            mutableStateOf(
+                getTextValueForCell(cellValue)
+            )
+        }
+        Text(cellTextValue, autoSize = TextAutoSize.StepBased())
+    }
+}
