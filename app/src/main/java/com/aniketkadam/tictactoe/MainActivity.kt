19e8a6bf6b25e8dbfe59fbe817181ef8a3b4244b
Aniket Kadam <anikadamg@gmail.com>
2026-09-17 14:45:24 +0530

    Cleanup

--- app/src/main/java/com/aniketkadam/tictactoe/MainActivity.kt
+++ app/src/main/java/com/aniketkadam/tictactoe/MainActivity.kt
@@ -99,14 +99,6 @@ fun GameUi(
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GridPreview() {
--- app/src/main/java/com/aniketkadam/tictactoe/TicTacToeVm.kt
+++ app/src/main/java/com/aniketkadam/tictactoe/TicTacToeVm.kt
@@ -34,18 +34,25 @@ class TicTacToeVm(val winConditionUseCase: WinConditionUseCase = WinConditionUse
     // When the player moves, the grid is mutated to update the cell with a move for the current player.
     fun playerMove(cellIndex: Int) {
         _uiState.update { state ->
+            // Once the game ends, ignore clicks.
             if(state.winState != WinState.InProgress)
                 return
+
             // Add the move to the grid
             val updatedGrid = state.gridState.mutate {
                 it[cellIndex] = state.currentPlayer.toCellType()
             }
 
+            // Toggle the current player
+            val curPlayer = if(state.currentPlayer == CurrentPlayer.X) CurrentPlayer.O else CurrentPlayer.X
+
             // Check if the game is won/draw/inprogress
             val updatedGameState = winConditionUseCase.checkWinCondition(updatedGrid, cellIndex)
 
             // Set the updated state
-            state.copy(gridState = updatedGrid, winState = updatedGameState, currentPlayer = if(state.currentPlayer == CurrentPlayer.X) CurrentPlayer.O else CurrentPlayer.X)
+            state.copy(gridState = updatedGrid,
+                winState = updatedGameState,
+                currentPlayer = curPlayer)
         }
     }
 
